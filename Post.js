d39ee800161ce91fd25ab95e1fbb755ca8fd7814
Jungmin-Hong <jungmin011107@gmail.com>
2023-02-08 20:40:55 +0900

    feat: Add post API #2

--- Post.js
+++ Post.js
@@ -19,3 +19,56 @@ BigInt.prototype.toJSON = function() {
   return this.toString()
 }
 
+//게시글 작성 api
+app.post('/post', /*authenticateAccessToken,*/ async(req, res) => {
+    try{
+        // 파라미터 받아오기
+    const userId = req.body.userId;                  
+    const postTitle = req.body.postTitle;
+    const postContent = req.body.postContent;
+    const imagePath = req.body.imagePath;
+    const publishDate = req.body.publishDate;
+    const tag = req.body.tag;
+    const postViews = req.body.postViews;
+    const postLikes = req.body.postLikes;
+    //const datas = [name, title, content, passwd]; // 모든 데이터의 배열
+
+    await prisma.posts.create({
+        data:{
+            userId : userId,
+            postTitle : postTitle,
+            postContent : postContent,
+            imagePath : imagePath,
+            publishDate : publishDate,
+            tag : tag,
+            postViews : postViews,
+            postLikes : postLikes
+        },
+    })
+    res.send({message:'Saved Succesfully'})
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send({error: 'Server Error'});
+    }
+});
+
+
+//게시글 상세 조회 api
+app.get('/post/read/:id', async (req, res) => {
+    try{
+        const postId = Number(req.params.id)
+
+        const post = await prisma.posts.findUnique({
+            where:{
+                postId : postId
+            },
+        });
+        res.send(post);
+      }
+      catch(error){
+        console.error(error);
+        res.status(500).send({error: 'Server Error.'});
+      }
+});
+
